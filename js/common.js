245e3c0e64b6c950c2b903f61cc25b87406abd2c
jungpil5770 <jungpil5770@gmail.com>
2023-01-20 11:48:56 +0900

    로그인 페이지 완성

---
+++ js/common.js
@@ -0,0 +1,24 @@
+
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+})
+
+// 검색창 요소 내부 실제 input 요소에 포커스가 되면 실행
+searchInputEl.addEventListener('focus',function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색')
+})
+
+// 검색창 요소 내부 실제 input 요소에 포커스가 해제(블러)되면 실행
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder',"");
+})
+
+//올해가 몇 년도인지 계산
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear()
+//                         날짜 중 년도만 가져오겠다
--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-})
-
-// 검색창 요소 내부 실제 input 요소에 포커스가 되면 실행
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색')
-})
-
-// 검색창 요소 내부 실제 input 요소에 포커스가 해제(블러)되면 실행
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder',"");
-})
 
 const badgeEl = document.querySelector('header .badges')
 const toTopEl = document.querySelector('.to-top')
@@ -30,7 +12,7 @@ window.addEventListener('scroll', _.throttle(function(){//._.throttle 로데시 
   if(window.scrollY>500){
 
 //배지 숨기기
-//ㅎ\gsap.to(요소, 지속시간, 옵션)
+//gsap.to(요소, 지속시간, 옵션)
   gsap.to(badgeEl, .6, {
     opacity: 0,
     display: 'none'
@@ -43,7 +25,7 @@ window.addEventListener('scroll', _.throttle(function(){//._.throttle 로데시 
     })
 
   }else{
-   //배지 보이기
+   //뱃지 보이기
    gsap.to(badgeEl, .6, {
      opacity: 1,
      display : 'block'
@@ -151,8 +133,5 @@ spyEls.forEach(function(spyEl){
   .addTo(new ScrollMagic.Controller())
 })
 
-//올해가 몇 년도인지 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear()
-//                         날짜 중 년도만 가져오겠다
+
 
